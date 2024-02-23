ca427e3aa39d824e3a2b169d7d5b52a7450d0b56
Drift24 <ongjiasiong24@gmail.com>
2024-02-23 18:18:46 +0800

    AUDIO MANAGER

--- WIU/Assets/Folder_JiaSiong/Script/VolumeManager.cs
+++ WIU/Assets/Folder_JiaSiong/Script/VolumeManager.cs
@@ -1,30 +1,43 @@
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.Audio;
+    using UnityEngine;
+    using UnityEngine.UI;
+    using UnityEngine.Audio;
 
 public class VolumeManager : MonoBehaviour
 {
-    public Slider volumeSlider;
-    public AudioMixer audioMixer; // Reference to the Audio Mixer
+    [SerializeField] private AudioMixer audioMixer;
 
-    void Start()
+    public void SetVolume(float sliderValue)
     {
-        if (volumeSlider != null)
-        {
-            volumeSlider.value = PlayerPrefs.GetFloat("Volume", 1f);
-            OnVolumeChanged(); // Call the method to apply the initial volume
-        }
-    }
-
-    // Called when the slider value changes
-    public void OnVolumeChanged()
-    {
-        float volume = volumeSlider.value;
-
-        // Save the volume level to PlayerPrefs for future sessions
-        PlayerPrefs.SetFloat("Volume", volume);
-
-        // Set the exposed parameter on the Audio Mixer
-        audioMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(sliderValue) * 20);
     }
 }
+
+//        public Slider volumeSlider;
+//        public AudioMixer audioMixer; // Reference to the Audio Mixer
+//    void Start()
+//    {
+//        if (volumeSlider != null)
+//        {
+//            volumeSlider.value = PlayerPrefs.GetFloat("Volume", 1f);
+
+//            // Connect the OnVolumeChanged method to the slider's OnValueChanged event
+//            volumeSlider.onValueChanged.AddListener(OnVolumeChanged);
+
+//            OnVolumeChanged(volumeSlider.value); // Call the method to apply the initial volume
+//        }
+//    }
+
+//    // Corrected signature for the OnVolumeChanged method
+//    public void OnVolumeChanged(float value)
+//    {
+//        Debug.Log("Volume Slider Value: " + value);
+
+//        float volume = value;
+
+//        // Save the volume level to PlayerPrefs for future sessions
+//        PlayerPrefs.SetFloat("Volume", volume);
+
+//        // Set the exposed parameter on the Audio Mixer
+//        audioMixer.SetFloat("Master", Mathf.Log10(volume) * 20);
+//    }
+//}
